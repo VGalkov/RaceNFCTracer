bd828c5b396ce7d0d3d5730eef4dc8d08f56d088
Vladimir Galkov <s0506777@yandex.ru>
2018-11-15 10:59:08 +0400

    блядь заебался тут совмещать комиты с разных компов. похоже потеряна работа 2х дней и нужно будет ещё раз всё синхронихировать.

--- app/src/main/java/ru/galkov/racenfctracer/MainActivity.java
+++ app/src/main/java/ru/galkov/racenfctracer/MainActivity.java
@@ -78,11 +78,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     // Client settings =================================================================
     //
+
     public static int SERVER_PORT = 8080;
     //    public static int SERVER_PORT = 8095;
-    public static String server =  "192.168.1.5"; // "127.0.0.1";
+//    public static String server =  "192.168.1.5"; // "127.0.0.1";
     public static String serverPort = Integer.toString(SERVER_PORT);
-    //public static String server =  "185.251.240.3";
+    public static String server =  "185.251.240.3";
+
     //  192.168.1.5:8080
     // на самом деле это интикатор версии. иначе и не используется. в случае расхождения версий сервера и клиента
     // клиент не работает.
